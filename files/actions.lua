df3c68f45b8f6299ebfee4b061a7b2b822233010
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2025-09-22 21:54:15 -0400

    tweak probabilities and naming; add 3-cast

--- files/actions.lua
+++ files/actions.lua
@@ -10,8 +10,8 @@ table.insert( actions,
     description        = "$actiondesc_array_2",
     sprite             = "mods/array_formations/files/array_2.png",
 	type 			   = ACTION_TYPE_DRAW_MANY,
-	spawn_level        = "0,1,2,3,4,5,6", -- BURST_2
-	spawn_probability  = "0.5,0.6,0.7,0.7,0.7,0.7,0.7", -- BURST_2
+	spawn_level        = "1,2,3,4,5,6",             -- BURST_3
+	spawn_probability  = "0.5,0.6,0.7,0.7,0.7,0.7", -- BURST_3
 	price = 180,
 	mana = 6,
 	--max_uses = 100,
@@ -23,13 +23,31 @@ table.insert( actions,
 
 table.insert( actions,
 {
+    id                 = "ARRAY_3",
+    name               = "$action_array_3",
+    description        = "$actiondesc_array_3",
+    sprite             = "mods/array_formations/files/array_3.png",
+	type 			   = ACTION_TYPE_DRAW_MANY,
+	spawn_level        = "2,3,4,5,6",           -- BURST_4
+	spawn_probability  = "0.3,0.4,0.6,0.6,0.6", -- BURST_4
+	price = 220,
+	mana = 9,
+	--max_uses = 100,
+	action = function()
+		c.spread_degrees = c.spread_degrees - 9.0
+		array_cast(3)
+	end,
+} )
+
+table.insert( actions,
+{
     id                 = "ARRAY_4",
     name               = "$action_array_4",
     description        = "$actiondesc_array_4",
     sprite             = "mods/array_formations/files/array_4.png",
 	type 			   = ACTION_TYPE_DRAW_MANY,
-	spawn_level        = "2,3,4,5,6", -- BURST_4
-	spawn_probability  = "0.4,0.5,0.6,0.6,0.6", -- BURST_4
+	spawn_level        = "3,4,5,6",         -- BURST_5?
+	spawn_probability  = "0.3,0.4,0.5,0.5", -- BURST_5?
 	price = 220,
 	mana = 12,
 	--max_uses = 100,
